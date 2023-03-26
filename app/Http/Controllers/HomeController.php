307b79d48dc1d55b6c9815bc90845386f9cbf524
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-26 14:45:38 -0300

    Guzzlereq

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -12,6 +13,27 @@ class HomeController extends Controller
     }
 
     public function ingredientes(Request $request): View{
-        dd($request);
+        $client = new Client([
+            'base_uri' => 'https://api.openai.com/v1/completions',
+            'headers' => [
+                'Content-type' => 'application/json',
+                'Authorization' => 'Bearer '.env('GPT_KEY')
+            ]
+        ]);
+
+        $response = $client->post('completions', [
+            'json' => [
+                'model' => 'text-davinci-003',
+                'prompt' => 'Gere a melhor receita com os seguintes ingredientes: '.$request->ingredientes,
+                'temperature' => 0.5,
+                'max_tokens' => 500
+            ]
+        ]);
+
+        if($response->getStatusCode() == 200){
+            $data = json_decode($response->getBody(), true);
+        }
+
+        dd($data);
     }
 }
