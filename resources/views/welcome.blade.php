1af9f306471187295ef6804fb3b41becf28d5605
João Vitor Mendonça <91424123+jvittortrikx1997@users.noreply.github.com>
2023-03-26 12:42:04 -0300

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-GTP 🤖</title>
+    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+</head>
+<body>
+    <header>
+        <h1>Laravel - GPT 🤖</h1>
+    </header>
+    <main>
+        <p>Principal</p>
+    </main>
+    <footer>
+        Laravel - GPT
+    </footer>
+</body>
+</html>
